fbe3d0fc6bb0600cb677782292280e12ad092d25
lab314-6@labeng.local <lab314-6@labeng.local>
2020-12-15 15:58:37 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
+    int n=1,even=0,odd=0;
+    while(n!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin>>n;
+    if(n!=0){
+    if(n%2==0){
+        even++;
+    }
+    else{
+        odd++;
+    }
+    }
+    }
+    cout << "#Even numbers = "<<even<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
